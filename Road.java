593ad94fa4ca6f764d232103e5a96d6427f4123e
Perjési Anna Katinka <btfu2i@inf.elte.hu>
2020-03-19 12:26:43 +0100

    Upload New File

---
+++ Road.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package catchthebus;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author Anyud
+ */
+public class Road extends Sprite {
+
+    public Road(int x, int y, int width, int height, Image image) {
+        super(x, y, width, height, image);
+    }
+    
+}
