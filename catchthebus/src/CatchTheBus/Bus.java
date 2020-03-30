c2371648f2184236b45d6c9c283a38143025ebe9
Perjési Anna Katinka <btfu2i@inf.elte.hu>
2020-03-30 23:02:53 +0200

    Anna new changes

--- catchthebus/src/CatchTheBus/Bus.java
+++ catchthebus/src/CatchTheBus/Bus.java
@@ -5,6 +5,7 @@ import java.awt.Image;
 public class Bus extends Sprite {
     // Anna commentje
 	// Peti commentje
+    // Anna megint új commentje
     public Bus(int x, int y, int width, int height, Image image) {
         super(x, y, width, height, image);
     }
