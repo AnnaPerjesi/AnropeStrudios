bf67bb281f27c866251e847a91bee325f3e58bc5
Perjési Anna Katinka <btfu2i@inf.elte.hu>
2020-03-30 23:05:37 +0200

    Anna próba

--- catchthebus/src/CatchTheBus/Bus.java
+++ catchthebus/src/CatchTheBus/Bus.java
@@ -6,6 +6,7 @@ public class Bus extends Sprite {
     // Anna commentje
 	// Peti commentje
     // Anna megint új commentje
+    // Anna netbeans commentje
     public Bus(int x, int y, int width, int height, Image image) {
         super(x, y, width, height, image);
     }
