57dc6841fb6d01b24b9ec67f49a9aa794c6b6ed3
Perjési Anna Katinka <btfu2i@inf.elte.hu>
2020-04-22 09:43:11 +0200

    BUG actionListener

--- catchthebus/src/model/GameGUI.java
+++ catchthebus/src/model/GameGUI.java
@@ -142,12 +142,12 @@ public class GameGUI extends JFrame {
                             delBtn.setBackground(new java.awt.Color(220, 0, 0));
                             delBtn.setBorder(BorderFactory.createLineBorder(Color.black));
                             delBtn.setVisible(true);
-                            /*Lehet nem csak ennyi*/
+                            /*BUG rengeteg*/
                             delBtn.addActionListener(new ActionListener() {
                             @Override
                             public void actionPerformed(ActionEvent e) {
-                                //realTowers.remove(temp);
                                 gameArea.sellTower(temp, type);
+                                
                                 twLevel.setVisible(false);
                                 twPower.setVisible(false);
                                 twRange.setVisible(false);
